9c64ff4c820868beee4cdb966cedad9dec03f3e4
Felop2299 <pipe-220899@hotmail.com>
2025-02-12 22:58:20 +0000

    falta el front

--- src/api/models.py
+++ src/api/models.py
@@ -1,12 +1,24 @@
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 db = SQLAlchemy()
+bcrypt = Bcrypt()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    is_active = db.Column(db.Boolean(), unique=False, nullable=False, default=True)
+
+    def __init__(self,email,password, is_active=True):
+        self.email=email
+        self.password=bcrypt.generate_password_hash(password).decode('utf-8')
+        self.is_active=is_active
+
+     
+    def check_password(self,password):
+        return bcrypt.check_password_hash(self.password,password)
+
 
     def __repr__(self):
         return f'<User {self.email}>'
@@ -15,5 +27,6 @@ class User(db.Model):
         return {
             "id": self.id,
             "email": self.email,
+            "is_active":self.is_active
             # do not serialize the password, its a security breach
         }
--- src/api/routes.py
+++ src/api/routes.py
@@ -5,18 +5,56 @@ from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
+from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
+from flask_bcrypt import Bcrypt
 
+bcrypt = Bcrypt()
 api = Blueprint('api', __name__)
 
 # Allow CORS requests to this API
 CORS(api)
 
 
-@api.route('/hello', methods=['POST', 'GET'])
-def handle_hello():
+@api.route('/signup', methods=['POST'])
+def signup():
+    data = request.get_json()
+    email = data.get("email")
+    password = data.get("password")
 
-    response_body = {
-        "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
-    }
+    if not email or not password:
+        return jsonify({"error": "Email and password required"}), 400
 
-    return jsonify(response_body), 200
+    # Verificar si el usuario ya existe
+    if User.query.filter_by(email=email).first():
+        return jsonify({"error": "the user already exist"}), 409
+
+    # Crear usuario y guardar en la DB
+    hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')
+    new_user = User(email=email, password=password, is_active=True)
+    db.session.add(new_user)
+    db.session.commit()
+
+    return jsonify({"message": "user succesfully register"}), 201
+
+@api.route("/login",methods=["POST"])
+def login():
+    data=request.get_json()
+    email=data.get("email")
+    password=data.get("password")
+
+    user=User.query.filter_by(email=email).first()
+    if not user or not user.check_password(password):
+        return jsonify({"error":"incorrect credentials"}),401
+
+    acces_token=create_access_token(identity=str(user.id))
+    return jsonify({"token":acces_token,"message":"login successful"}),200
+
+@api.route("/validate-token", methods=["GET"])
+@jwt_required()
+def validate_token():
+    user_id=get_jwt_identity()
+    user = User.query.get(user_id)
+    if not user:
+        return jsonify({"error": "User not found"}), 404
+    
+    return jsonify({"message":"valid token","user_id":user.serialize()}),200   
